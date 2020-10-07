59d34fd08033b498aad928b4d8d7eaf53ed67735
UxDeveloper82 <electronicsmf@aol.com>
2020-10-06 22:04:53 -0400

    using Map and useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const [post, setPosts] = useState([
+     {
+       username: "Martin F",
+       caption: "This is Amazing",
+       imageUrl: "http://icreatesites4u.com/Image/img_31-05-2020-19-09-17.png"
+     },
+     {
+        username: "Martin B",
+        caption: "Another Amazing thing",
+        imageUrl: "http://icreatesites4u.com/Image/img_31-05-2020-19-09-17.png"
+     }
+  ]);
   return (
     <div className="App">
         <div className="app__header">
@@ -10,8 +22,12 @@ function App() {
               src="https://pngimage.net/wp-content/uploads/2018/06/nome-instagram-png-4.png" alt=""/>
         </div>
         <h1>Hello From Icreatesites4u</h1>
-        
-        <Post username="Martin F" caption="Wow it works" imageUrl="http://icreatesites4u.com/Image/img_31-05-2020-19-09-17.png"/>
+
+        { 
+          post.map(post => {
+             return <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          })
+        }
     </div>
   );
 }
