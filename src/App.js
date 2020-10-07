ed8a540a9b014199ea32f1b8c0732896e1cf0473
UxDeveloper82 <electronicsmf@aol.com>
2020-10-06 20:06:20 -0400

    Add style and image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-         <h1>Hello From Icreatesites4u</h1>
+        <div className="app__header">
+            <img className="app__headerImage" 
+              src="https://pngimage.net/wp-content/uploads/2018/06/nome-instagram-png-4.png" alt=""/>
+        </div>
+        <h1>Hello From Icreatesites4u</h1>
+
+        {/* Posts */}
+        {/* Posts */}
     </div>
   );
 }
